71f979451ce55d12bec369830c6af1c6426d8ba1
acreilly <acreilly3@aol.com>
2014-05-12 22:50:26 -0400

    Added my_solutions

--- week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
+++ week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
@@ -8,18 +8,46 @@
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? an array of numbers
+# What is the output? (i.e. What should the code return?) adding up the numbers in the array
 # What are the steps needed to solve the problem?
+# Define a method called total
+# create a variable called number and set it to zero
+# create a for loop that takes each element in the array to do i
+# set the variable number to equal number plus i
+# return number after loop
 
+# What is the input? an array of strings
+# What is the output? (i.e. What should the code return?) joining the strings, making sure the sentence is capitalized and adding a period at the end
+# What are the steps needed to solve the problem?
+# take the parameter array and add the method capitalize
+# take the parameter array and add the method join
+# add the string of a period to array
 
 # 2. Initial Solution
 
+def total(array) 
+  number = 0
+  array.each do |i|
+    number += i
+  end
+  number
+end
 
+puts total([1,2,3])
+
+
+def sentence_maker(array)
+array.first.capitalize!
+array.join(" ") + "."
+end
+
+puts sentence_maker(["i", "want", "to", "go", "to", "the", "movies"]) 
 
 
 # 3. Refactored Solution
-
+# I believe this was the simplest way to do the problem
 
 
 # 4. Reflection 
+# I need help at first, but once I got the idea I got the problems correct. I really need to review ruby before I go further in this weeks tasks.
