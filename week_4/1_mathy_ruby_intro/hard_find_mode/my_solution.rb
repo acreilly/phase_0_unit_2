c17b7d67b366635b2205dea151c60a4e5620e117
acreilly <acreilly3@aol.com>
2014-05-12 23:37:04 -0400

    hard find mode pseudocode

--- week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
+++ week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
@@ -8,13 +8,24 @@
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? an array of number or strings
+# What is the output? (i.e. What should the code return?) If all parts of the array are unique, it will return the whole array. If there are duplicates or not unique, it returns the not unique part of the array.
 # What are the steps needed to solve the problem?
+# Define a method called mode
+# give it parameter of array
+# create a loop that checks each element of the array
+# check if array is empty
+# write if statement that checks if none of the array is similar to return whole array
+# if statement that checks if any elements are similar to return those elements
 
 
 # 2. Initial Solution
 
+def mode(array)
+for array.each do |i|
+
+end
+end
 
 
 
