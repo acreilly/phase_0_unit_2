1180ece5c5fc3238ad0e4e553ca3f7f2146c3912
acreilly <acreilly3@aol.com>
2014-05-15 17:06:56 -0400

    Reflection added

--- week_4/4_nums_commas_solo_challenge/my_solution.rb
+++ week_4/4_nums_commas_solo_challenge/my_solution.rb
@@ -86,3 +86,31 @@ puts separate_comma(9900000)
 puts separate_comma(0)       
 puts separate_comma(100)
 # 4. Reflection 
+=begin
+What parts of your strategy worked? What problems did you face?
+At first I just did comparisons and inserted the comma from the right
+but when it came time to insert the comma twice, it wouldn't work.
+There was also an issue with the numbers so I decided to change the if statement
+into finding anything within a range
+What questions did you have while coding? What resources did you find to help you answer them?
+I had to research how to insert a string. At first I thought it was
+inject but found out it was insert. I used ruby docs to find it.
+Also to find a range or how to go about it I used http://stackoverflow.com/questions/870507/determining-if-a-variable-is-within-range-ruby
+What concepts are you having trouble with, or did you just figure something out? If so, what?
+I had to research a lot, so I didn't have trouble with the concepts
+once I researched how to go about it.
+Did you learn any new skills or tricks?
+I learned how to find a range with .between? I also learned that
+with string it is .insert not .inject
+How confident are you with each of the Learning Competencies?
+Decently confident. I know there is another way to go about solving this
+problem, maybe with a loop. If I have free time later this week
+I would like to figure out a way to do any number and insert a comma every 3 characters
+	I unfortunately can only go up to 999 million unless I want to keep writing if statements forever
+Which parts of the challenge did you enjoy?
+Solving the problem and feeling confident with the things I learned
+Which parts of the challenge did you find tedious?
+I just wish I could figure out how to make it do every three characters without
+	writing if statements forever.
+
+=end
