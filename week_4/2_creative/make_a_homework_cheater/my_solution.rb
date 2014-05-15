9b4f5fa361f845e3876a1ec077c9636d5ccf8991
acreilly <acreilly3@aol.com>
2014-05-15 14:12:16 -0400

    Template, Pseudocode added

--- week_4/2_creative/make_a_homework_cheater/my_solution.rb
+++ week_4/2_creative/make_a_homework_cheater/my_solution.rb
@@ -1,13 +1,23 @@
 # U2.W4: Homework Cheater!
+# User stories
+# As a user I want to fill in the parameters with values that will
+# flow easily with the paragraph I wrote.
 
+#template
+#Blank is a great piece of art. It was created by blank the year blank.
+# It is considered to be blank style and if sold today would be worth blank.
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself ].
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input: parameters that have been given values
+# Output: Filled in values for the parameters that creates a paragraph of information.
 # Steps:
+# define method essay_writer
+# Give parameters of piece, artist, year, style, worth
+# Create puts statement that returns a string adding the parameters in correctly.
+# Call method outside of method
 
 
 # 3. Initial Solution
