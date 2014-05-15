d18bfb097a41da871dea61f0f8e57c3090beb610
acreilly <acreilly3@aol.com>
2014-05-15 14:12:26 -0400

    Letter grade solution added

--- week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
+++ week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
@@ -8,16 +8,40 @@
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? an array of numbers
+# What is the output? (i.e. What should the code return?) the average of those numbers
 # What are the steps needed to solve the problem?
+# define method of get_grade
+# create loop
+#add each element together
+# divide by length
+#find grade through if statement
 
 
 # 2. Initial Solution
 
+def get_grade(array)
+sum = 0
+array.each do |i|
+sum += i
+end
+average = sum / array.length
+if average >= 90
+	"A"
+elsif average >= 80
+	"B"
+elsif average >= 70
+	"C"
+elsif average >= 60
+	"D"
+elsif average < 59
+	"F"
+else
+	"error"
+end
+end
 
-
-
+puts get_grade([90,50,100])
 # 3. Refactored Solution
 
 
