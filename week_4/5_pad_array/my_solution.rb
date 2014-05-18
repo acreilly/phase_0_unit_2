313292461904c6ebbe5286df77f65d4eec8d76fe
acreilly <acreilly3@aol.com>
2014-05-18 14:39:36 -0400

    Initial solution

--- week_4/5_pad_array/my_solution.rb
+++ week_4/5_pad_array/my_solution.rb
@@ -4,22 +4,46 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 1. Pseudocode
 
-# What is the input?
+# What is the input? an array with a minimum size
 # What is the output? (i.e. What should the code return?)
+# an array that is at no smaller than min size
 # What are the steps needed to solve the problem?
-
+# Create a class called Array
+# Within the array class create pad! method
+	#
 
 # 2. Initial Solution
 
+class Array
+
+	def pad!(min_size, pad_value = nil)
+		while self.length < min_size
+			self.push(pad_value)
+		end
+	self
+	end
+
+def pad(min_size, pad_value = nil)
+@arr = self.clone
+	while @arr.length < min_size
+		@arr.push(pad_value)
+	end
+@arr
+end
+
+end
+
+#Refactored Solution
 
 
 
-# 3. Refactored Solution
+#Driver Code
+puts [1,2,3].pad(5) == [1,2,3,nil,nil]
+puts [1,2,3].pad(5, 'apple') == [1,2,3,'apple', 'apple']
+puts [1,2,3].pad(0) == [1,2,3]
 
-
-
-# 4. Reflection 
+#Reflection
