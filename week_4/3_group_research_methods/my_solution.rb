6ae07c55cf838f1ff14101205fa66d95f6011e7e
acreilly <acreilly3@aol.com>
2014-05-18 16:41:30 -0400

    Added Reflection, Person 1's solution

--- week_4/3_group_research_methods/my_solution.rb
+++ week_4/3_group_research_methods/my_solution.rb
@@ -5,7 +5,26 @@ my_family_pets_ages = {"Evi" => 6, "Hoobie" => 3, "George" => 12, "Bogart" => 4,
             "Annabelle" => 0, "Ditto" => 3}
 
 # Person 1's solution
+# Person 1's solution
+def my_array_finding_method(source, thing_to_find)
+  array = []
+  source.each do |x|
+    if x.include?(thing_to_find)
+      array.push(x)
+    end
+  end
+  p array
+end
 
+def my_hash_finding_method(source, thing_to_find)
+  array = []
+  source.each do |key, value|
+    if value == thing_to_find
+      array.push(key)
+    end
+  end
+  p array
+end
 # Identify and describe the ruby method you implemented. 
 # 
 #
@@ -75,3 +94,30 @@ end
 
 p my_array_modification_method(i_want_pets, 1) == ["I", "want", 4, "pets", "but", "I", "only", "have", 3 ]
 p my_hash_modification_method(my_family_pets_ages, 2) == {"Evi" => 8, "Hoobie" => 5, "George" => 14, "Bogart" => 6, "Poly" => 6, "Annabelle" => 2, "Ditto" => 5}
+
+
+
+=begin
+  REFLECTION 
+What parts of your strategy worked? What problems did you face?
+I used map which worked. I was trying to use each_value for hash but couldn't
+figure out a way to get it to work.
+What questions did you have while coding? What resources did you find to help you answer them?
+How to modify the values of the hash using each_value but
+did not find a solution.
+What concepts are you having trouble with, or did you just figure something out? If so, what?
+I had to research how to do the hash, I don't
+  completely understand why I would take the key of each key and then
+  equal it to the value plus the thing to modify.
+
+Did you learn any new skills or tricks?
+Map
+
+How confident are you with each of the Learning Competencies?
+Confident, other than the each_value method.
+Which parts of the challenge did you enjoy?
+Figuring it out. I struggled with this challenge
+Which parts of the challenge did you find tedious?
+that I couldn't figure out a way to use each_value
+
+=end
