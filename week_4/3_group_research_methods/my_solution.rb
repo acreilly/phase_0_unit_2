fe732fa9c77dc847cffd2998cbe34cf3e1e5d49c
acreilly <acreilly3@aol.com>
2014-05-18 16:36:31 -0400

    Person 2 Solution

--- week_4/3_group_research_methods/my_solution.rb
+++ week_4/3_group_research_methods/my_solution.rb
@@ -13,25 +13,30 @@ my_family_pets_ages = {"Evi" => 6, "Hoobie" => 3, "George" => 12, "Bogart" => 4,
 
 # Person 2
 def my_array_modification_method(source, thing_to_modify)
-  source.each do |i|
-  	if i.integer? == true
+  source.map! do |i|
+  	if i.is_a? Integer
   		i += thing_to_modify
-  	else
-  		p source
+    else
+      i
   end
-  p source
 end
+  source
 end
+
 
 
 def my_hash_modification_method(source, thing_to_modify)
-  # Your code here!
+  source.each do |x,y|
+    source[x] = y += thing_to_modify
+  end
+  source
 end
 
 # Identify and describe the ruby method you implemented. 
-# 
-#
-#
+# I used .map! so I could look into each element of the array and be able to modify it
+#if it were an integer, if not just keep as is.
+# For hash, I used .each which is nothing new.. I had to do research on how to modify the value
+# I tried using .each_value but I couldn't figure out how to get it to do the same thing.
 
 =begin
 # Person 3
@@ -68,12 +73,5 @@ end
 # HINT: Use `puts` statements to see if you are altering the original structure with these methods. 
 # Each of these should return `true` if they are implemented properly.
 
-p my_array_finding_method(i_want_pets, "t") == ["want","pets","but"]
-p my_hash_finding_method(my_family_pets_ages, 3) == ["Hoobie", "Ditto"]
 p my_array_modification_method(i_want_pets, 1) == ["I", "want", 4, "pets", "but", "I", "only", "have", 3 ]
 p my_hash_modification_method(my_family_pets_ages, 2) == {"Evi" => 8, "Hoobie" => 5, "George" => 14, "Bogart" => 6, "Poly" => 6, "Annabelle" => 2, "Ditto" => 5}
-p my_array_sorting_method(i_want_pets) == ["3", "4", "I", "but", "have", "only", "pets", "want"]
-p my_hash_sorting_method(my_family_pets_ages) == [["Annabelle", 2], ["Ditto", 5], ["Hoobie", 5], ["Bogart", 6], ["Poly", 6], ["Evi", 8], ["George", 14]] 
-# This may be false depending on how your method deals with ordering the animals with the same ages.
-p my_array_deletion_method(i_want_pets, "a") == ["I", 4, "pets", "but", "I", "only", 3 ]
-p my_hash_deletion_method(my_family_pets_ages, "George") == {"Evi" => 8, "Hoobie" => 5, "Bogart" => 6, "Poly" => 6, "Annabelle" => 2, "Ditto" => 5}
