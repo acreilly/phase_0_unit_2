c565b02d1727ef1ce70b43f014c89acbdd74f240
acreilly <acreilly3@aol.com>
2014-05-17 23:07:54 -0400

    unfinished

--- week_4/3_group_research_methods/my_solution.rb
+++ week_4/3_group_research_methods/my_solution.rb
@@ -5,13 +5,6 @@ my_family_pets_ages = {"Evi" => 6, "Hoobie" => 3, "George" => 12, "Bogart" => 4,
             "Annabelle" => 0, "Ditto" => 3}
 
 # Person 1's solution
-def my_array_finding_method(source, thing_to_find)
-  # Your code here!
-end
-
-def my_hash_finding_method(source, thing_to_find)
-  # Your code here!
-end
 
 # Identify and describe the ruby method you implemented. 
 # 
@@ -20,8 +13,16 @@ end
 
 # Person 2
 def my_array_modification_method(source, thing_to_modify)
-  # Your code here!
+  source.each do |i|
+  	if i.integer? == true
+  		i += thing_to_modify
+  	else
+  		p source
+  end
+  p source
 end
+end
+
 
 def my_hash_modification_method(source, thing_to_modify)
   # Your code here!
@@ -32,7 +33,7 @@ end
 #
 #
 
-
+=begin
 # Person 3
 def my_array_sorting_method(source)
   # Your code here!
@@ -56,7 +57,7 @@ end
 def my_hash_deletion_method(source, thing_to_delete)
   #Your code here!
 end
-
+=end
 # Identify and describe the ruby method you implemented. 
 # 
 #
@@ -76,10 +77,3 @@ p my_hash_sorting_method(my_family_pets_ages) == [["Annabelle", 2], ["Ditto", 5]
 # This may be false depending on how your method deals with ordering the animals with the same ages.
 p my_array_deletion_method(i_want_pets, "a") == ["I", 4, "pets", "but", "I", "only", 3 ]
 p my_hash_deletion_method(my_family_pets_ages, "George") == {"Evi" => 8, "Hoobie" => 5, "Bogart" => 6, "Poly" => 6, "Annabelle" => 2, "Ditto" => 5}
-
-# Reflect!
-# 
-# 
-# 
-# 
-# 
