2624c73143602c58b2a46e3eb21787e7e67c3c06
acreilly <acreilly3@aol.com>
2014-05-21 14:27:04 -0400

    Added Initial and Refactored Solution

--- week_5/3_guessing_game_solo_challenge/my_solution.rb
+++ week_5/3_guessing_game_solo_challenge/my_solution.rb
@@ -5,32 +5,86 @@
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input: integer
+# Output: if the guess is higher lower or correct
 # Steps:
+# attr_accessor added
+# have instance variable answer equal answer
+# create instance variable correct and set false for default
+
+#Define guess method with parameter of integer
+	#if integer is less than answer
+		#return low & return correct as false
+	#if integer is higher than answer
+		#return high and return correct as false
+	# else return correct and return correct as true
+
+#define solved
+	#return @correct
+
 
 
 # 3. Initial Solution
 
+=begin
 class GuessingGame
+	attr_accessor :answer
   def initialize(answer)
-    # Your initialization code goes here
+  	@answer = answer
+  	@correct = false
+  	end
+
+  def guess(int)
+  	if int > @answer
+  		@correct = false
+  		return :high
+  	elsif int < @answer
+  		@correct = false
+  		return :low
+  	else
+  		@correct = true
+  		return :correct
   end
-  
-  def guess
-    
-  end
+end
   
   def solved?
-    
+  	@correct
   end
+
 end
 
 
+=end
 
 
 # 4. Refactored Solution
+#I realized you dont need attr accessor
+# changed parameter name to guess
 
+class GuessingGame
+  def initialize(answer)
+  	@answer = answer
+  	@correct = false
+  	end
+
+  def guess(guess)
+  	if guess > @answer
+  		@correct = false
+  		return :high
+  	elsif guess < @answer
+  		@correct = false
+  		return :low
+  	else
+  		@correct = true
+  		return :correct
+  end
+end
+  
+  def solved?
+  	@correct
+  end
+
+end
 
 
 
@@ -38,13 +92,15 @@ end
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
-game.solved? == false
-game.guess(5) == :low
-game.guess(20) == :high
-game.solved? == false
-game.guess(10) == :correct
-game.solved? == true
+game = GuessingGame.new(10)
 
+p game.solved? == false
+p game.guess(5) == :low
+p game.guess(20) == :high
+p game.solved? == false
+
+p game.guess(10) == :correct
+p game.solved? == true
 
 
 
